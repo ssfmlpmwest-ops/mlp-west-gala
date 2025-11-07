ed578dd3875abf0f0423d2f497b7167ee43ad383
usmanmt786 <baumonmt786@gmail.com>
2025-11-07 16:43:40 +0530

    updated school list

--- lib/conts.ts
+++ lib/conts.ts
@@ -14,604 +14,85 @@ export const DIVISIONS = [
 ];
 export const COURSES = ["Commerce", "Science", "Humanities", "VHSE"];
 export const SCHOOLS = [
-  {
-    name: "College of Applied Science, Malappuram",
-  },
-  {
-    name: "College of Applied Science, Vattamkulam",
-  },
-  {
-    name: "College of Applied Science, Vazhakkad",
-  },
-  {
-    name: "College of Applied Science, Muthuvallur",
-  },
-  {
-    name: "Technical Higher Secondary School, Perinthalmanna",
-  },
-  {
-    name: "Technical Higher Secondary School, Vattamkulam",
-  },
-  {
-    name: "Technical Higher Secondary School, Vazhakkad",
-  },
-  {
-    name: "Study Centre, Valanchery",
-  },
-  {
-    name: "Extension Centre, Tirur",
-  },
-  {
-    name: "Extension Centre, Thavnur",
-  },
-  {
-    name: "Government Polytechnic College, Perinthalmanna",
-  },
-  {
-    name: "Government Polytechnic College, Thirurangadi",
-  },
-  {
-    name: "Government Women's Polytechnic College, Kottakkal",
-  },
-  {
-    name: "Government Polytechnic College, Manjeri",
-  },
-  {
-    name: "MGM Technological Campus, Valiyaparambu, Valanchery",
-  },
-  {
-    name: "Ma'din College of Engineering and Management, Melmuri",
-  },
-  {
-    name: "KMCT Polytechnic College, Malappuram",
-  },
-  {
-    name: "Orphanage Polytechnic College, Malappuram",
-  },
-  {
-    name: "Malabar Polytechnic College, Kottakkal",
-  },
-  {
-    name: "Majlis Polytechnic College, Puramannur",
-  },
-  {
-    name: "Devaki Amma's Guruvayoorappan College of Architecture, Chelambra",
-  },
-  {
-    name: "MGM College of Engineering and Pharmaceutical Sciences, VALANCHERY",
-  },
-  {
-    name: "Eranad Knowledge City Technical Campus, Manjeri",
-  },
-  {
-    name: "MEA Engineering College, Perinthalmanna",
-  },
-  {
-    name: "MES College of ARTS , Trikkanapuram",
-  },
-  {
-    name: "Vedavyasa Institute of Technology, Ponnempadam",
-  },
-  {
-    name: "Malabar Islamic Complex Arts & Science College",
-  },
-  {
-    name: "MM Knowledge Arts and Science College, Karakund",
-  },
-  {
-    name: "Khansa Women's College for Advanced Studies",
-  },
-  {
-    name: "M. E. S. College",
-  },
-  {
-    name: "Ideal Arabic College",
-  },
-  {
-    name: "Devaki Amma Memorial College of Pharmacy",
-  },
-  {
-    name: "Jamia Salafiya Pharmacy College",
-  },
-  {
-    name: "M.E.S Medical College",
-  },
-  {
-    name: "MIMS College of Nursing",
-  },
-  {
-    name: "Moulana college of nursing and Paramedical sciences",
-  },
-  {
-    name: "Kelappaji College of Agricultural Engineering & Technology, Thavanur (KCoAET Thavanur)",
-  },
-  {
-    name: "Cashew Research Station, Anakkayam",
-  },
-  {
-    name: "KVK, Tavanur",
-  },
-  {
-    name: "Government College, Malappuram",
-  },
-  {
-    name: "Pookoya Thangal Memorial Government College, Perinthalmanna, Malappuram (est. 1975)",
-  },
-  {
-    name: "Thunchan Memorial Government College, Tirur, Malappuram (est. 1980)",
-  },
-  {
-    name: "Govt. Arts and Science College, Nilambur",
-  },
-  {
-    name: "N.S.S College, Manjeri",
-  },
-  {
-    name: "Dr. Gafoor Memorial MES Mampad College",
-  },
-  {
-    name: "Pocker Sahib Memorial Orphanage College (PSMO College), Malappuram (est. 1968)",
-  },
-  {
-    name: "M.E.S. Ponnani College, Ponnani, Malappuram (est. 1968)",
-  },
-  {
-    name: "Mar Thoma College, Chungathara",
-  },
-  {
-    name: "Korambayil Ahammed Haji Memorial Unity Women's College, Manjeri, Malappuram (est. 1981)",
-  },
-  {
-    name: "MES Keveeyam College, Valanchery, Malappuram (est. 1981)",
-  },
-  {
-    name: "EMEA College of Arts and Science, Kumminiparamba",
-  },
-  {
-    name: "Sullamussalam Science College, Areekode",
-  },
-  {
-    name: "Amal College of Advanced Studies, Nilambur",
-  },
-  {
-    name: "Majlis Arts & Science College Valanchery, Malappuram (est. 1995)",
-  },
-  {
-    name: "SNDP Yogam Sathabdi Smaraka College (also SNDP YSS College) Perinthalmanna, Malappuram (est. 2002)",
-  },
-  {
-    name: "HM College of Science & Technology, Manjeri",
-  },
-  {
-    name: "Markaz Arts & Science College, Malappuram (est. 2003)",
-  },
-  {
-    name: "K.V. Ustad Memorial Darul Hidaya Orphanage Arts & Science College For Women, Kololamba, Malappuram (est. 2003)",
-  },
-  {
-    name: "Sree Vivekananda Padana Kendram Arts & Science College, Edakkara, Malappuram (est. 2003)",
-  },
-  {
-    name: "Regional College of Science and Humanities, Kuzhimanna",
-  },
-  {
-    name: "Najath College of Science and Technology, Malappuram (est. 2003)",
-  },
-  {
-    name: "Grace Valley College of Arts & Science, Malappuram (est. 2003)",
-  },
-  {
-    name: "Vedavyasa Institute of Arts & Science, Vazhayur, Malappuram",
-  },
-  {
-    name: "Assabah Arts and Science College, Valayamkulam, Malappuram (est. 2005)",
-  },
-  {
-    name: "Kottakkal Farook Arts and Science College, Malappuram (est 2005)",
-  },
-  {
-    name: "Khidmath Arts and Science College, Malappuram (est. 2005)",
-  },
-  {
-    name: "SAFI Institute of Advanced Study, Vazhayur",
-  },
-  {
-    name: "Gems Arts & Science College, Ramapuram",
-  },
-  {
-    name: "Sree Sastha College, Malappuram (est. 2008)",
-  },
-  {
-    name: "Ma'din Arts and Science College, Melmuri",
-  },
-  {
-    name: "Safa College of Arts and Science, Malappuram (est. 2009)",
-  },
-  {
-    name: "HiCer College of Advanced studies, Pathiriyal",
-  },
-  {
-    name: "Ideal College for Advanced Studies, Thavanoor, Malappuram (est. 2010)",
-  },
-  {
-    name: "Al Jamia Arts & Science College, Malappuram",
-  },
-  {
-    name: "St. Mary's College, Angadippuram",
-  },
-  {
-    name: "Blossom Arts & Science College, Kondotty",
-  },
-  {
-    name: "Noble Women's College, Manjeri, Malappuram",
-  },
-  {
-    name: "Pravasi Arts & Science College, Malappuram (est. 2011)",
-  },
-  {
-    name: "IKT Memorial Arts & Science College, Malappuram (est. 2012)",
-  },
-  {
-    name: "Priyadarsini Arts & Science College, Ernad",
-  },
-  {
-    name: "Bafakhy Yatheem Khana Arts & Science College for Women, Malappuram (est. 2012)",
-  },
-  {
-    name: "CPA College of Arts and Science, Puthanathani, Malappuram (est. 2012)",
-  },
-  {
-    name: "MIC Arts & Science College, Valluvambram",
-  },
-  {
-    name: "Nasra College of Arts & Science, Thirurkad, Malappuram (est. 2012)",
-  },
-  {
-    name: "ISS Arts & Science College, Perinthalmanna",
-  },
-  {
-    name: "CH Muhammed Koya Memorial Govt. Arts & Science College, Tanur, Malappuram (est. 2013)",
-  },
-  {
-    name: "Sahya Arts and Science College, Wandoor",
-  },
-  {
-    name: "Panakkad Pookoya Thangal Memorial Arts & Science College, Vengara, Malappuram",
-  },
-  {
-    name: "Malabar College of Commerce & Science, Malappuram (est. 2013)",
-  },
-  {
-    name: "Malabar College of Advanced Studies, Vengara, Malappuram (est. 2013)",
-  },
-  {
-    name: "Government Arts & Science College, Kondotty",
-  },
-  {
-    name: "Govt. Arts & Science College, Mankada, Kolathur",
-  },
-  {
-    name: "Eranad Knowledge City College of Commerce and Sciences, Manjeri, Malappuram",
-  },
-  {
-    name: "JM College of Arts & Science, Tirur, Malappuram",
-  },
-  {
-    name: "Moulana College of Arts & Science & Commerce, Tirur, Malappuram (est. 2013)",
-  },
-  {
-    name: "KMCT Arts & Science College, Kuttippuram, Malappuram (est. 2013)",
-  },
-  {
-    name: "Fathima Arts & Science College, Nilambur",
-  },
-  {
-    name: "MES Arts and Science College, Nilambur, Malappuram â€“ no longer functioning",
-  },
-  {
-    name: "Mohammed Ali Shihab Thangal Memorial Arts and Science College (MSTM), Perinthalmanna, Malappuram",
-  },
-  {
-    name: "MES Arts and Science College, Perinthalmanna, Malappuram",
-  },
-  {
-    name: "Government Arts and Science College, Post, Malappuram (est. 2014)",
-  },
-  {
-    name: "Women's Islamiya Arts and Science College, Punnappala Post, Malappuram (est. 2014)",
-  },
-  {
-    name: "M.T.M College of Arts, Science and Commerce, Ponnani Taluk, Malappuram (est. 2014)",
-  },
-  {
-    name: "Ambedkar College of Arts and Science, Wandoor",
-  },
-  {
-    name: "KR's Sree Narayana College, Valanchery, Malappuram (est. 2014)",
-  },
-  {
-    name: "Panakkad Mohamedali Shihab Thangal Arts and Science College, Nannambra, Malappuram (est. 2015)",
-  },
-  {
-    name: "Vedavyasa College of Arts and Science, Eranad Taluk",
-  },
-  {
-    name: "Jamia Islamiya Arts and Science College, Manjeri",
-  },
-  {
-    name: "MAO College of Arts and Science, Elayur",
-  },
-  {
-    name: "Government Arts and Science College for Women, Downhill, Malappuram (est. 2015)",
-  },
-  {
-    name: "Sayyid Muhammad Ali Shihab Thangal Memorial Arts and Science Women's College, Malappuram",
-  },
-  {
-    name: "Luminous Arts and Science College, Malappuram",
-  },
-  {
-    name: "Ability Arts & Science College for Hearing Impaired, Valiyaparamba, Malappuram",
-  },
-  {
-    name: "College of Applied Science, Munduparamba, Malappuram (est. 1993)",
-  },
-  {
-    name: "college of applied science calicut (est 1993)",
-  },
-  {
-    name: "College of Applied Sciences, Edappal, Malappuram (est. 2005)",
-  },
-  {
-    name: "College of Applied Sciences, Vazhakkad, Malappuram (est. 2005)",
-  },
-  {
-    name: "College of Applied Sciences, Ernad Taluk, Malappuram (est. 2010)",
-  },
-  {
-    name: "Model Degree College, Parappanangadi, Malappuram (est. 2015)",
-  },
-  {
-    name: "MES College of Engineering, Kuttippuram, Malappuram (est. 1994)",
-  },
-  {
-    name: "MEA Engineering College, Perinthalmanna, Malappuram (est. 2002)",
-  },
-  {
-    name: "Vedavyasa Institute of Technology, Karadparamba, Malappuram (est. 2004)",
-  },
-  {
-    name: "Devaki Amma's Guruvayurappan College of Architecture, Malappuram (est. 2010)",
-  },
-  {
-    name: "Alsalama Institute of Architecture, Perinthalmanna",
-  },
-  {
-    name: "Cochin College of Engineering and Technology, Valanchery, Malappuram (est. 2012)",
-  },
-  {
-    name: "Talent Institute of Management Studies, Edappal, Malappuram (est. 2013)",
-  },
-  {
-    name: "Eranad Knowledge City Technical Campus (EKC), Cherukulam, Manjeri, Malappuram",
-  },
-  {
-    name: "Vedavyasa College of Architecture, Karadparamba, Malappuram (est. 2014)",
-  },
-  {
-    name: "Eranad Knowledge City College of Architecture, Cherukulam, Manjeri",
-  },
-  {
-    name: "Talent Institute of Architecture, Malappuram (est. 2015)",
-  },
-  {
-    name: "Monti International Institute of Management Studies, Angadippuram, Malappuram (est. 2015)",
-  },
-  {
-    name: "Markaz Training College, Karthala, Malappuram (est. 1995)",
-  },
-  {
-    name: "MCT Training College, Melmuri, Malappuram (est. 1995)",
-  },
-  {
-    name: "KPPM College of Teacher Education, Anakkayam, Malappuram (est. 2002)",
-  },
-  {
-    name: "Devaki Amma Memorial College of Teacher Education, Chelembra, Malappuram (est. 2002)",
-  },
-  {
-    name: "Kunhathumma Memorial College of Teacher Education, Valillapuzha, Malappuram (est. 2003)",
-  },
-  {
-    name: "Sree Vivekananda College of Teacher Education, Edakkara, Malappuram (est. 2004)",
-  },
-  {
-    name: "Moulana College of Teacher Education, Kuttayi, Malappuram (est. 2004)",
-  },
-  {
-    name: "Farook B.Ed College, Kottakkal, Malappuram (est. 2004)",
-  },
-  {
-    name: "Bafakhy Yatheemkhana B.Ed Training College, Kalpakanchery, Malappuram (est. 2004)",
-  },
-  {
-    name: "Majmaâ€™a Training College, Kavanur, Malappuram (est. 2004)",
-  },
-  {
-    name: "EMEA Training College, Kondotty, Malappuram (est. 2004)",
-  },
-  {
-    name: "Sullamussalam College of Teacher Education, Malappuram (est. 2004)",
-  },
-  {
-    name: "ISS College of Teacher Education, Perinthalmanna, Malappuram (est. 2006)",
-  },
-  {
-    name: "M.I Training College, Malappuram (est. 2006)",
-  },
-  {
-    name: "Darul Uloom Training College, Vazhakkad",
-  },
-  {
-    name: "Bhavan's N.A. Palkhivala Academy for Advanced Legal Studies and Research (est. 2012)",
-  },
-  {
-    name: "KMCT Law College, Valanchery, Malappuram (est. 2013)",
-  },
-  {
-    name: "MCT College of Legal Studies, Melmuri, Malappuram",
-  },
-  {
-    name: "Madeenathul Uloom Arabic College, Pulikkal",
-  },
-  {
-    name: "Ansar arabic college, Valavannur, Malappuram",
-  },
-  {
-    name: "Darul Uloom Arabic College, Vazhakkad",
-  },
-  {
-    name: "Anvarul Islam Arabic College, Kizhuparamba",
-  },
-  {
-    name: "Sullamussalam Arabic College, Malappuram",
-  },
-  {
-    name: "Anwarul Islam Women's Arabic College Mongam, Malappuram",
-  },
-  {
-    name: "Isha â€“ Athul Islam Arabic College, Parappanangadi, Malappuram (est. 2003)",
-  },
-  {
-    name: "Falahiya Arabic College, Down Hill, Malappuram (est. 2003)",
-  },
-  {
-    name: "Al-Hidayath Arabic College, Malappuram (est. 2003)",
-  },
-  {
-    name: "Assabah Arabic College, Malappuram (est. 2003)",
-  },
-  {
-    name: "Ilahiya Arabic College, Tirurkad, Malappuram (est. 2010)",
-  },
-  {
-    name: "KMMMO Arabic College, Thirurangadi, Malappuram (est. 2010)",
-  },
-  {
-    name: "Jamia Nadawiyya Women's Arabic College, Edavanna, Malappuram (est. 2012)",
-  },
-  {
-    name: "Shareeath College for Women, Thenhipalam, Malappuram",
-  },
-  {
-    name: "Safa Arabic College, Pookkattiri, Edayur",
-  },
-  {
-    name: "Thunchath Ezhuthachan Malayalam University",
-  },
-  {
-    name: "GVHSS Omanur",
-  },
-  {
-    name: "GHSS Kuzhimanna",
-  },
-  {
-    name: "G.H.S. Chullikkode",
-  },
-  {
-    name: "GovT High SChool MuThuvallur",
-  },
-  {
-    name: "GLPS Kizhisseri",
-  },
-  {
-    name: "GLPS Kuzhimanna",
-  },
-  {
-    name: "GLPS Kuzhimanna II South",
-  },
-  {
-    name: "GLPS Parappur",
-  },
-  {
-    name: "GLPS Parapurathparamba",
-  },
-  {
-    name: "GLPS ParaThakkad",
-  },
-  {
-    name: "GLPS Ponnad",
-  },
-  {
-    name: "GMLPS PookalaThur",
-  },
-  {
-    name: "GLPS Pulpatta",
-  },
-  {
-    name: "GMLPS Thavanur",
-  },
-  {
-    name: "GLPS Valamangalam",
-  },
-  {
-    name: "GLPS Vilayil",
-  },
-  {
-    name: "GLPS Vilayil Parappur",
-  },
-  {
-    name: "GUPS CheaCode",
-  },
-  {
-    name: "GUPS Kadungallur",
-  },
-  {
-    name: "GMUPS Kizhisseri",
-  },
-  {
-    name: "MGLC Melmuri",
-  },
-  {
-    name: "KKMHS Cheekode",
-  },
-  {
-    name: "CHMHS Pookolathur",
-  },
-  {
-    name: "AMLPS CherupuThur",
-  },
-  {
-    name: "PMSAPTMALPS Chirappalam",
-  },
-  {
-    name: "AMLPS Cholayilmukku",
-  },
-  {
-    name: "AMLPS Mundakkulam",
-  },
-  {
-    name: "ALPS Mundamparamba",
-  },
-  {
-    name: "AMLPS Muthanoor",
-  },
-  {
-    name: "PMSALPS MuThuparamba",
-  },
-  {
-    name: "AMLPS Olamathil",
-  },
-  {
-    name: "AMLPS Omanur",
-  },
-  {
-    name: "AMLPS Palakkad",
-  },
-  {
-    name: "PMSAPTMLPS Palakkad NorTh",
-  },
-  {
-    name: "AMLPS Panat",
-  },
+  { name: "DHOHSS POOKARATHARA" },
+  { name: "GHSS EDAPPAL" },
+  { name: "GHSS KADANJERY" },
+  { name: "GHSS KOKKUR" },
+  { name: "GHSS THAVANOOR" },
+  { name: "PCNGHSS MOOKUTHALA" },
+  { name: "GHSS OTHUKKUNGAL" },
+  { name: "GOVT RAJAS KOTTAKKAL" },
+  { name: "HSS KOTTUR" },
+  { name: "HSS PUTHUPPARAMB" },
+  { name: "KHMHSS VALAKKULAM" },
+  { name: "PKMMHSS EDARIKODE" },
+  { name: "VHSS CHAPPANANGADI" },
+  { name: "B E M H S S PARAPPANANGADI" },
+  { name: "C B H S S PARAPPANANGADI" },
+  { name: "M V H S S ARIYALLUR" },
+  { name: "SNMHSS PARAPPANANGADI" },
+  { name: "AV HSS THRIKKAVU" },
+  { name: "GHSS MUKKALA" },
+  { name: "GHSS PALAPETTY" },
+  { name: "GHSS VANNERI" },
+  { name: "GHSS VELIANCODE" },
+  { name: "MES PONNANI" },
+  { name: "MI HSS PONNANI" },
+  { name: "BYKHSS VALAVANNUR" },
+  { name: "CHSS CHERULAL" },
+  { name: "GHSS MATTUMMAL" },
+  { name: "GVHSS KALPAKANCHERY" },
+  { name: "MSMHSS KALLINGALPARAMB" },
+  { name: "NMHSS TIRUNNAVAYA" },
+  { name: "VVMHSS VATTAPARAMB" },
+  { name: "CPPHSS AYYAYA" },
+  { name: "DGHSS DHEVATHAR" },
+  { name: "GHSS KATILANGADI" },
+  { name: "GHSS MANGADU" },
+  { name: "GRFT VHSS" },
+  { name: "SMMHSS RAYIRIMANGALAM" },
+  { name: "CHSS VELIMUKK" },
+  { name: "GHSS PERUVALLUR" },
+  { name: "GMHSS UNIVERSITY" },
+  { name: "GVHSS CHELARI" },
+  { name: "MHSS MOONNIYUR" },
+  { name: "NAJATH HSS PERUVALLUR" },
+  { name: "NNMHSS CHELEMBRA" },
+  { name: "CO OPERATIVE COLLEGE TIRUR" },
+  { name: "GAID TIRUR" },
+  { name: "GBHSS THRKANDIYUR" },
+  { name: "GHSS CHERIYAMUNDAM" },
+  { name: "GHSS EZHUR" },
+  { name: "GHSS PURATHUR" },
+  { name: "GVHSS PARAVANNA" },
+  { name: "KHMHSS ALATHIYUR" },
+  { name: "MALABAR HIGHER SECONDARY SCHOOL" },
+  { name: "MMHSS KUTTAYI" },
+  { name: "NAVODAYA COLLEGE ALATHIYUR" },
+  { name: "UDAYA COLLEGE ALATHIYUR" },
+  { name: "AR NAGAR HIGHER SECONDARY SCHOOL, CHENDAPURAYA" },
+  { name: "CO-OPERATIVE HIGHER SECONDARY SCHOOL, TIRURANGADI" },
+  { name: "GOVT. HIGHER SECONDARY SCHOOL,TIRURANGADI" },
+  { name: "KMHSS KUTTOOR NORTH" },
+  { name: "ORIENTAL HIGHER SECONDARY SCHOOL, TIRURANGADI" },
+  { name: "SEETHI SAHIB MEMORIAL HIGHER SECONDARY SCHOOL, TIRURANGADI" },
+  { name: "GVHSS PERUMANNA" },
+  { name: "GVHSS PONMUDAM" },
+  { name: "BHSS MAVANDIYOUR" },
+  { name: "GHSS IRIMBILYAM" },
+  { name: "GHSS KUTTIPURAM" },
+  { name: "GHSS PERSHANUR" },
+  { name: "MES VENDALOOR" },
+  { name: "VHSS VALANCHERY" },
+  { name: "AL - IHSAN HSS VENGARA" },
+  { name: "CO - OPERATIVE COLLEGE VENGARA" },
+  { name: "GMVHSS VENGARA" },
+  { name: "GVHSS VENGARA" },
+  { name: "IU ARTS PARAPPUR" },
+  { name: "IUHSS PARAPPUR" },
+  { name: "MALABAR COLLEGE HSS KUTTALUR" },
+  { name: "MAMBAHUL HUDA HSS KUZHICHENA" },
+  { name: "MUHSS OORAKAM" },
+  { name: "PPTM COMMERCE HSS CHERUR" },
+  { name: "PPTMYHSS CHERUR" },
 ];
